bf620ab01ccf8ce99085dac489b8b1fec4a9fe47
Fuica <fransiskus_mario87@outlook.com>
2022-03-27 19:32:01 +0700

    Finish Static Pages and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		base_title = "Twitter KW-5"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
